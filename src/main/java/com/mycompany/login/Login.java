2a57779dbf6e542153e6e4075c1020cd7b6b2d2e
Carlos <Carlos@DESKTOP-NQEV2F5>
2023-12-29 18:56:33 -0300

    Update

---
+++ src/main/java/com/mycompany/login/Login.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.login;
+
+import com.mycompany.login.igu.Principal;
+
+/**
+ *
+ * @author Carlos
+ */
+public class Login {
+
+    public static void main(String[] args) {
+           Principal princ = new Principal();
+           princ.setVisible(true);
+           princ.setLocationRelativeTo(null);//sin referencia y pone en medio
+    }
+}
---
+++ src/main/java/com/mycompany/login/logica/Usuario.java
@@ -0,0 +1,45 @@
+
+package com.mycompany.login.logica;
+
+public class Usuario {
+    private int id;
+    private String nombreUsuario;
+    private String contrasenia;
+
+    public Usuario() {
+    }
+
+    public Usuario(int id, String nombreUsuario, String contrasenia) {
+        this.id = id;
+        this.nombreUsuario = nombreUsuario;
+        this.contrasenia = contrasenia;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public String getContrasenia() {
+        return contrasenia;
+    }
+
+    public void setContrasenia(String contrasenia) {
+        this.contrasenia = contrasenia;
+    }
+    
+    
+    
+    
+}
